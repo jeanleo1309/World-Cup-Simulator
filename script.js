fe1765f76d70d58fd5d5d78f6a9c78940adcc92f
Jean Leal <135157454+jeanleo1309@users.noreply.github.com>
2026-04-16 13:25:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+async function loadTeams(){
 
+const response = await fetch(
+"https://development-internship-api.geopostenergy.com/WorldCup/GetAllTeams",
+{
+headers:{
+"git-user":"seu_usuario_github"
+}
+}
+)
+
+const teams = await response.json()
+
+console.log(teams)
+
+const container = document.getElementById("teams")
+
+container.innerHTML = ""
+
+teams.forEach(team => {
+
+const p = document.createElement("p")
+
+p.innerText = team.name
+
+container.appendChild(p)
+
+})
+
+}
