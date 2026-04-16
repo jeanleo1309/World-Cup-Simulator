eb4a62df79481f3551e071dae7b5b6617d897b72
Jean Leal <135157454+jeanleo1309@users.noreply.github.com>
2026-04-16 13:33:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,16 @@ container.appendChild(p)
 })
 
 }
+function shuffle(array){
+
+for(let i = array.length - 1; i > 0; i--){
+
+const j = Math.floor(Math.random() * (i + 1))
+
+[array[i], array[j]] = [array[j], array[i]]
+
+}
+
+return array
+
+}
