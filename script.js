6dd46fb1f558787315264f543fd2b7498e1e3ea2
Jean Leal <135157454+jeanleo1309@users.noreply.github.com>
2026-04-16 17:01:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -42,3 +42,73 @@ const j = Math.floor(Math.random() * (i + 1))
 return array
 
 }
+function createGroups(teams){
+
+const shuffled = shuffle(teams)
+
+const groups = {}
+
+const letters = ["A","B","C","D","E","F","G","H"]
+
+let index = 0
+
+letters.forEach(letter => {
+
+groups[letter] = shuffled.slice(index, index + 4)
+
+index += 4
+
+})
+
+return groups
+
+}
+function displayGroups(groups){
+
+const container = document.getElementById("teams")
+
+container.innerHTML = ""
+
+for(let group in groups){
+
+const div = document.createElement("div")
+
+const title = document.createElement("h2")
+
+title.innerText = "Group " + group
+
+div.appendChild(title)
+
+groups[group].forEach(team =>{
+
+const p = document.createElement("p")
+
+p.innerText = team.name
+
+div.appendChild(p)
+
+})
+
+container.appendChild(div)
+
+}
+
+}
+async function loadTeams(){
+
+const response = await fetch(
+"https://development-internship-api.geopostenergy.com/WorldCup/GetAllTeams",
+{
+headers:{
+"git-user":"SEU_USUARIO"
+}
+}
+)
+
+const teams = await response.json()
+
+const groups = createGroups(teams)
+
+displayGroups(groups)
+
+}
